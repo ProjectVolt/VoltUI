23d3fbd887a248cce100680512ab24ab4ef4d651
Kacper Stolarek <stolarek83@gmail.com>
2024-03-15 15:35:57 +0100

    added the hero component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from '@/components/landing/Header';
+import { Hero } from '@/components/landing/Hero';
 
-export default async function HomePage() {
+export default async function HomePage({ params: { locale } }: { params: { locale: string } }) {
   return (
     <>
       <Header />
+      <Hero locale={locale} />
     </>
   );
 }
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -8,6 +8,8 @@ module.exports = {
         'mantine-breakpoint-md': '62em',
         'mantine-breakpoint-lg': '75em',
         'mantine-breakpoint-xl': '88em',
+        'mantine-breakpoint-xxl': '92em',
+        'volt-side-margin-xl': '95px',
         'volt-side-margin-lg': '80px',
         'volt-side-margin-sm': '30px',
       },
