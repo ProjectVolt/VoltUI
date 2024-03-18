9d6bc97af3d2f12ba72fffe04cf6a9357a743fbb
Kacper Stolarek <stolarek83@gmail.com>
2024-03-18 15:52:11 +0100

    added the open-source component

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import { Cards } from '@/components/landing/Cards';
 import { Details } from '@/components/landing/Details';
 import { Header } from '@/components/landing/Header';
 import { Hero } from '@/components/landing/Hero';
+import { OpenSource } from '@/components/landing/OpenSource';
 
 export default async function HomePage({ params: { locale } }: { params: { locale: string } }) {
   return (
@@ -10,6 +11,7 @@ export default async function HomePage({ params: { locale } }: { params: { local
       <Hero locale={locale} />
       <Cards locale={locale} />
       <Details locale={locale} />
+      <OpenSource locale={locale} />
     </>
   );
 }
---
+++ components/landing/OpenSource/index.tsx
@@ -0,0 +1,18 @@
+import { Button } from '@mantine/core';
+import { IoLogoGithub } from 'react-icons/io5';
+import classes from './opensource.module.css';
+import initTranslations from '@/app/i18n';
+
+export async function OpenSource({ locale }: { locale: string }) {
+  const { t } = await initTranslations(locale, ['landing']);
+  return (
+    <div className={classes.container}>
+      <h1>{t('opensource-title')}</h1>
+      <h2>{t('opensource-description')}</h2>
+      <Button variant="filled" color="black" size="xl" radius="xl">
+        <IoLogoGithub style={{ marginRight: '10px' }} />
+        GitHub
+      </Button>
+    </div>
+  );
+}
